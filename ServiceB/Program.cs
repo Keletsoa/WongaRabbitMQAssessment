700a238279cb7e6e6c086659fd421d3caf6014e8
Keletso Rafedile <Keletso.Rafedile@arcelormittal.com>
2022-10-20 12:45:38 +0200

    Working on ServiceB - Receiver

--- ServiceB/Program.cs
+++ ServiceB/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RabbitMQ.Client;
+using RabbitService;
+
+RabbitMessagingService messagingService = new RabbitMessagingService();
+IConnection connection = messagingService.GetConnection();
+IModel model = connection.CreateModel();
+messagingService.ReceiveMessage(model);
