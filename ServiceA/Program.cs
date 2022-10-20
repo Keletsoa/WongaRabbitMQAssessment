2417c27f0910ce932ab52510948636aa0788b9c9
Keletso Rafedile <Keletso.Rafedile@arcelormittal.com>
2022-10-20 12:44:37 +0200

    Working on ServiceA

--- ServiceA/Program.cs
+++ ServiceA/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RabbitService;
+
+RabbitMessagingService messagingService = new RabbitMessagingService();
+var connection = messagingService.GetConnection();
+var model = connection.CreateModel();
+//messagingService.SetUpQueue(model);
+
+Console.WriteLine("Enter your name and press Enter. Quit with 'exit'.");
+while (true)
+{
+    string message = Console.ReadLine();
+    if (message.ToLower() == "exit") break;
+
+    messagingService.SendMessage(message, model);
+}
+
+Console.ReadKey();
