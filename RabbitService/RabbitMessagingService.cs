99b0cfb8b0096fab54bc46a2840c302f9c1f9557
Keletso Rafedile <Keletso.Rafedile@arcelormittal.com>
2022-10-20 12:43:20 +0200

    Working on service

---
+++ RabbitService/RabbitMessagingService.cs
@@ -0,0 +1,72 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitService
+{
+    public class RabbitMessagingService
+    {
+        private string _hostName = "localhost";
+        private string _userName = "guest";
+        private string _password = "guest";
+        private string _exchangeName = "";
+        private string _queueName = "Wonga Communicator";
+        private string _routinKey = "wongaRabbit";
+        private bool _durable = false;
+        private bool _exclusive = false;
+        private bool _autoDelete = false;
+
+        public IConnection GetConnection()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            connectionFactory.HostName = _hostName;
+            //connectionFactory.UserName = _userName;
+            //connectionFactory.Password = _password;
+
+            return connectionFactory.CreateConnection();
+        }
+
+        public void SetUpQueue(IModel model)
+        {
+            model.QueueDeclare(_queueName, _durable, false, false, null);
+        }
+
+        public void SendMessage(string message, IModel model)
+        {
+            model.QueueDeclare(queue: _queueName,
+                                durable: _durable,
+                                exclusive: _exclusive,
+                                autoDelete: _autoDelete,
+                                arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            model.BasicPublish(exchange: _exchangeName,
+                                routingKey: _routinKey,
+                                basicProperties: null,
+                                body: body);
+        }
+
+        public void ReceiveMessage(IModel model)
+        {
+            model.QueueDeclare(queue: _queueName,
+                                durable: _durable,
+                                exclusive: _exclusive,
+                                autoDelete: _autoDelete,
+                                arguments: null);
+
+            var consumer = new EventingBasicConsumer(model);
+            consumer.Received += (theModel, basicDeliverEventArgs) =>
+            {
+                var body = basicDeliverEventArgs.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Hello {0}, I am your father!", message);
+            };
+
+            model.BasicConsume(queue: _queueName,
+                                autoAck: true,
+                                consumer: consumer);
+            Console.ReadKey();
+        }
+    }
+}
